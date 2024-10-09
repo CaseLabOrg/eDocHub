2617dae122b01d73bc412cdc188001ea5e442645
Dmitrii Pligin <D-pligin@mail.ru>
2024-10-09 19:50:33 +0300

    migrations fix

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,40 +1,40 @@
 CREATE TABLE Documents(
-                          document_id SERIAL PRIMARY KEY,
+                          id BIGSERIAL PRIMARY KEY,
                           title VARCHAR(255),
-                          user_id INTEGER,
-                          type_id INTEGER,
+                          user_id BIGINT,
+                          type_id BIGINT,
                           description VARCHAR(255),
                           created_at DATE,
                           version INTEGER
 );
 
 CREATE TABLE Document_Types(
-                               type_id SERIAL PRIMARY KEY,
+                               id BIGSERIAL PRIMARY KEY,
                                name VARCHAR(255)
 );
 
 
 CREATE TABLE Attributes(
-                           attribute_id SERIAL PRIMARY KEY,
+                           id BIGSERIAL PRIMARY KEY,
                            document_type_id INTEGER,
                            name VARCHAR(255),
                            required BOOLEAN
 );
 
 CREATE TABLE Values(
-                       value_id SERIAL PRIMARY KEY,
+                       id BIGSERIAL PRIMARY KEY,
                        attribute_id INTEGER,
                        document_id INTEGER,
                        value VARCHAR(255)
 );
 
 CREATE TABLE Document_Content(
-                                 document_id INTEGER PRIMARY KEY,
+                                 id BIGSERIAL PRIMARY KEY,
                                  content VARCHAR(255)
 );
 
 CREATE TABLE Users(
-                      user_id SERIAL PRIMARY KEY,
+                      id BIGSERIAL PRIMARY KEY,
                       name VARCHAR(255),
                       surname VARCHAR(255),
                       email VARCHAR(255),
@@ -43,13 +43,13 @@ CREATE TABLE Users(
 );
 
 CREATE TABLE User_Roles(
-                           user_id INTEGER,
-                           role_id INTEGER,
+                           user_id BIGINT,
+                           role_id BIGINT,
                            constraint User_Roles_pk PRIMARY KEY(user_id, role_id)
 );
 
 CREATE TABLE Roles(
-                      role_id SERIAL PRIMARY KEY,
+                      id BIGSERIAL PRIMARY KEY,
                       name VARCHAR(255)
 );
 
