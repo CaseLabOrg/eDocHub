9bf6841d5341d5daa82b95c739d6fe7c5c05aff2
Dmitrii Pligin <D-pligin@mail.ru>
2024-10-16 15:37:59 +0300

    fix document operations

--- src/main/java/com/example/ecm/service/DocumentService.java
+++ src/main/java/com/example/ecm/service/DocumentService.java
@@ -2,12 +2,16 @@ package com.example.ecm.service;
 
 import com.example.ecm.dto.CreateDocumentRequest;
 import com.example.ecm.dto.CreateDocumentResponse;
-import com.example.ecm.dto.SignatureDto;
+import com.example.ecm.dto.CreateSignatureRequest;
+import com.example.ecm.exception.NotFoundException;
 import com.example.ecm.mapper.DocumentMapper;
 import com.example.ecm.mapper.SignatureMapper;
 import com.example.ecm.model.Document;
+import com.example.ecm.model.DocumentType;
 import com.example.ecm.model.Signature;
+import com.example.ecm.model.User;
 import com.example.ecm.repository.DocumentRepository;
+import com.example.ecm.repository.DocumentTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +27,7 @@ import java.util.Optional;
 public class DocumentService {
 
     private final DocumentRepository documentRepository;
+    private final DocumentTypeRepository documentTypeRepository;
     private final DocumentMapper documentMapper;
     private final SignatureMapper signatureMapper;
     private final MinioService minioService;
@@ -38,6 +43,8 @@ public class DocumentService {
      */
     public CreateDocumentResponse createDocument(CreateDocumentRequest createDocumentRequest) {
         Document document = documentMapper.toDocument(createDocumentRequest);
+        document.setUser(getUser(createDocumentRequest.getUserId()));
+        document.setDocumentType(getDocumentType(createDocumentRequest.getUserId()));
         Document documentSaved = documentRepository.save(document);
         boolean success = minioService.addDocument(documentSaved.getId(), createDocumentRequest);
         if (!success) {
@@ -60,7 +67,7 @@ public class DocumentService {
     public CreateDocumentResponse getDocumentById(Long id) {
         CreateDocumentResponse response = documentRepository.findById(id)
                 .map(documentMapper::toCreateDocumentResponse)
-                .orElseThrow(() -> new RuntimeException("Document not found"));
+                .orElseThrow(() -> new NotFoundException("Document not found"));
         response.setBase64Content(minioService.getBase64DocumentByName(response.getId() + "_" + response.getTitle()));
         return response;
     }
@@ -72,7 +79,7 @@ public class DocumentService {
      * @return список ответов с данными всех документов
      */
     public List<CreateDocumentResponse> getAllUserDocuments(String email) {
-        Long userId = userService.findByEmail(email).get().getId();
+        Long userId = userService.findByEmail(email).orElseThrow(() -> new NotFoundException("No such user")).getId();
         List<CreateDocumentResponse> list = documentRepository.findDocumentsBySignature(userId).stream()
                 .map(documentMapper::toCreateDocumentResponse)
                 .toList();
@@ -105,11 +112,11 @@ public class DocumentService {
      */
     public CreateDocumentResponse updateDocument(Long id, CreateDocumentRequest createDocumentRequest) {
         Document document = documentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Document not found"));
+                .orElseThrow(() -> new NotFoundException("Document not found"));
         minioService.deleteDocumentByName(document.getId() + "_" + document.getTitle());
         document.setTitle(createDocumentRequest.getTitle());
-        document.setUser(createDocumentRequest.getUser());
-        document.setDocumentType(createDocumentRequest.getDocumentType());
+        document.setUser(getUser(createDocumentRequest.getUserId()));
+        document.setDocumentType(getDocumentType(createDocumentRequest.getDocumentTypeId()));
         document.setDescription(createDocumentRequest.getDescription());
         document.setVersion(createDocumentRequest.getVersion());
         minioService.addDocument(id, createDocumentRequest);
@@ -122,13 +129,21 @@ public class DocumentService {
      * Добавляет подпись в документ.
      *
      * @param id           идентификатор документа
-     * @param signatureDto подпись
+     * @param createSignatureRequest подпись
      */
-    public void signDocument(Long id, SignatureDto signatureDto) {
+    public void signDocument(Long id, CreateSignatureRequest createSignatureRequest) {
         Document document = documentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Document not found"));
+                .orElseThrow(() -> new NotFoundException("Document not found"));
         List<Signature> signatures = document.getSignatures();
-        signatures.add(signatureMapper.toSignature(signatureDto));
+        signatures.add(signatureMapper.toSignature(createSignatureRequest));
         document.setSignatures(signatures);
+    }
+
+    private User getUser(Long id) {
+        return userService.findById(id).orElseThrow(() -> new NotFoundException("No such user"));
+    }
+
+    private DocumentType getDocumentType(Long id) {
+        return documentTypeRepository.findById(id).orElseThrow(() -> new NotFoundException("No such document type"));
     }
 }
