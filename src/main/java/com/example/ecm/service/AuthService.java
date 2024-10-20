03a6d8d2b95ac78dfe7dbabb348aaa957aa1dd59
Даниил Истомин <istomin.05.3@gmail.com>
2024-10-20 19:28:39 +0700

    Remove CI dependency from CD

--- src/main/java/com/example/ecm/service/AuthService.java
+++ src/main/java/com/example/ecm/service/AuthService.java
@@ -4,12 +4,14 @@ import com.example.ecm.dto.responses.LoginResponse;
 import com.example.ecm.security.UserPrincipal;
 import com.example.ecm.security.jwt.JwtIssuer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
@@ -18,6 +20,8 @@ public class AuthService {
     private final AuthenticationManager manager;
 
     public LoginResponse attemptLogin(String login, String password) {
+        log.info("Attempting to login using login: " + login);
+
         var auth = manager.authenticate(new UsernamePasswordAuthenticationToken(login, password));
 
         SecurityContextHolder.getContext().setAuthentication(auth);
