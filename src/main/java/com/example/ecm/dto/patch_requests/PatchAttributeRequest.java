ddd27703858b461a41ee319139019c9d53160a63
Nikita Gur <61457laize@gmail.com>
2024-10-23 23:41:40 +0700

    Added PATCH for Attribute #57

---
+++ src/main/java/com/example/ecm/dto/patch_requests/PatchAttributeRequest.java
@@ -0,0 +1,25 @@
+package com.example.ecm.dto.patch_requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PatchAttributeRequest {
+    /**
+     * Название типа документа, к которому относится атрибут
+     */
+    private List<String> documentTypesNames;
+
+    /**
+     * Имя атрибута, которое описывает его назначение
+     */
+    private String name;
+
+
+    private Boolean required;
+}
