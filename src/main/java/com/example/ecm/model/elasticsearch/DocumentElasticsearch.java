b3db659271071e2ad8789ddcbae1c39ce2464ce7
xfn3t <n.semenuta4830@gmail.com>
2024-11-05 12:31:43 +0300

    live document elastic

--- src/main/java/com/example/ecm/model/elasticsearch/DocumentElasticsearch.java
+++ src/main/java/com/example/ecm/model/elasticsearch/DocumentElasticsearch.java
@@ -44,4 +44,7 @@ public class DocumentElasticsearch {
 
     @Field(type = FieldType.Object)
     private Map<String, String> values;
+
+    @Field(type = FieldType.Boolean)
+    private Boolean isAlive;
 }
