6e098b8dce38f22b38c800b07acafd2c5c444577
Dmitrii Pligin <D-pligin@mail.ru>
2024-11-30 13:41:59 +0300

    fixing tenant

--- src/main/java/com/example/ecm/dto/responses/TenantResponse.java
+++ src/main/java/com/example/ecm/dto/responses/TenantResponse.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @Setter
 public class TenantResponse {
 
+    private Long id;
     private String name;
     private LocalDateTime createdAt;
     private boolean isAlive;
